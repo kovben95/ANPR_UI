0d4e370885d58d006e6efe786f1693d2c58bdfe6
Beni Kovács <kovacs.beni.1995@gmail.com>
2019-09-27 16:00:18 +0200

    initial commit: map there, axios imported to connect to rest api

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div style="height: 300px; width: 100%">
+        <l-map :zoom="zoom" :center="center">
+            <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+            <l-marker :lat-lng="marker"></l-marker>
+        </l-map>
+    </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        zoom: 13,
+        center: L.latLng(47.413220, -1.219482),
+        url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        marker: L.latLng(47.413220, -1.219482),
+      }
+    },
+  }
+</script>
