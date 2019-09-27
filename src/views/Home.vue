9683c2f9d35e668e14693f1bf878a16ae180bd11
Beni Kovács <kovacs.beni.1995@gmail.com>
2019-09-27 16:06:12 +0200

    initial commit: map there, axios imported to connect to rest api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,14 @@
     <div style="height: 300px; width: 100%">
         <l-map :zoom="zoom" :center="center">
             <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-            <l-marker :lat-lng="marker"></l-marker>
+            <l-marker v-for="(marker, i) in markers" :key="i" :lat-lng="marker"></l-marker>
         </l-map>
     </div>
 </template>
 
 <script>
 
+  const axios = require('axios');
 
   export default {
     name: 'Home',
@@ -18,8 +19,20 @@
         center: L.latLng(47.413220, -1.219482),
         url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        marker: L.latLng(47.413220, -1.219482),
+        markers: []
       }
     },
+    created() {
+      axios.get('/user?ID=12345')
+          .then((response) => {
+            console.log(response);
+            this.markers = response.data.points.map(p => L.latLng(p.lat, p.lng))
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+          .finally(function () {
+          });
+    }
   }
 </script>
