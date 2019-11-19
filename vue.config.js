ca2cdf4cf11db90a6a8a437bb46bc59dbf75dae5
Beni Kovács <kovacs.beni.1995@gmail.com>
2019-11-19 11:13:21 +0100

    update proxy + map center

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     headers: {'Access-Control-Allow-Origin': '*'},
     proxy: {
       '^/api': {
-        target: 'http://localhost:8000/api',
+        target: 'http://localhost:8000/',
         ws: true,
         changeOrigin: true
       },
